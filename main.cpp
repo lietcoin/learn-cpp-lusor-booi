ab8b20c31f9d87cf2e6d60d12431a2b37c6a3955
ltecoin <example@gmail.com>
2026-04-26 13:42:55 +0100

    refactor main.cpp to perform integer addition and subtraction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from Arch Linux" << std::endl;
+int main(){
+    // Load integer numebrs from input
+    int a{}, b{};
+
+    std::cout << "Enter an integer: ";
+    std::cin >> a;
+
+    std::cout << "Enter another integer: ";
+    std::cin >> b;
+
+    int sumPlus{a + b};
+    int sumMinus{a - b};
+
+    std::cout << "The sum of " << a << " and " << b << " is: " << sumPlus << ".\n";
+    std::cout << "The sum minus of " << a << " and " << b << " is: " << sumMinus << ".\n";
+
     return 0;
 }
