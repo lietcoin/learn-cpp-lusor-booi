5ec3ce8f25e7442116dbbf0e7f1d47d4c944b576
ltecoin <arturpanfil699@example.com>
2026-05-03 17:07:55 +0100

    learning sizeof

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
-#include "voltage.hpp"
 #include <iostream>
 
-
-
-int main()
-{
-    double voltage{getVoltage()};
-    double resistance{getResistance()};
-    double ResultCurrent{GetCurrent(voltage, resistance)};
-
-
-    PrintCurrent(ResultCurrent);
-    return 0;
+int main(){
+    std::cout << "HEy test test \n";
 }
--- voltage.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VOLTAGE_H
-#define VOLTAGE_H
-
-double getVoltage();
-double getResistance();
-double GetCurrent(double voltage, double resistance);
-void PrintCurrent(double current);
-
-#endif
