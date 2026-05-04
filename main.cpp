39968212cd0e424bb85b62ddae4c32d1e0625220
ltecoin <arturpanfil699@example.com>
2026-05-04 21:28:20 +0100

    finally learned how to make this proper, very happy with my self

--- main.cpp
+++ main.cpp
@@ -1,35 +1,46 @@
+// when ur back do the calculator again, remember void no return/ double, int and etc NEEDS and return variable
+// also remember to use an function for everything separatle
+
 #include <iostream>
 
 double getNumber(){
-	double num{};
-	std::cout << "Please insert a number of your choice: ";
-	std::cin >> num;
-	return num;
+    double number{};
+    std::cout << "Please insert an number: ";
+    std::cin >> number;
+    return number;
 }
 
 char getSymbol(){
-	char sym{};
-	std::cout << "Enter +, -, *, or /: ";
-	std::cin >> sym;
-	return sym;
+    char symbol;
+    std::cout << "Please insert an mathematical symbol: ";
+    std::cin >> symbol;
+    return symbol;
 }
 
-void Calculations(double first, double second, char symbol){
-	if (symbol == '+')
-		std::cout << first << symbol << second << " is " << first + second;
-	else if (symbol == '-')
-		std::cout << first << symbol << second << " is " << first - second;
-	else if (symbol == '*')
-		std::cout << first << symbol << second << " is " << first * second;
-	else if (symbol == '/')
-		std::cout << first << symbol << second << " is " << first / second;
-	else
-		std::cout << "error \n";
-}	
+double Calculations(double first, double second, char symbol){
+    if (symbol == '+')
+        return first + second;
+    else if (symbol == '-')
+        return first - second;
+    else if (symbol == '*')
+        return first * second;
+    else if (symbol == '/')
+        return first / second;
+    else
+        std::cout << "\"Something went wrong, expect to get answer 0.\" \n";
+        return 0;
+}
+
+void PrintResults(double first, double second, char symbol){
+    std::cout << first << symbol << second << " is equal to " << Calculations(first, second, symbol) << '\n';
+}
+
+
 
 int main(){
-	double first{ getNumber() };
-	double second{ getNumber() };
-	char symbol{ getSymbol() };
-	Calculations(first, second, symbol);
+    double first{ getNumber() };
+    double second{ getNumber() };
+    char symbol{ getSymbol() };
+    
+    PrintResults(first, second, symbol);
 }
