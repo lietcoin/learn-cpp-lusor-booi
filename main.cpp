f14b91fa86f595fb6adcb6a28ae437de2bdfaa02
ltecoin <arturpanfil699@example.com>
2026-05-11 15:38:01 +0100

    string_view learned

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
-// when ur back do the calculator again, remember void no return/ double, int and etc NEEDS and return variable
-// also remember to use an function for everything separatle
-
 #include <iostream>
-#include "calculator.hpp"
+#include <string>
+#include <string_view>
 
+std::string getName(int num){
+    std::string name{};
+    std::cout << "Enter the name of person #" << num << ": ";
+    std::getline(std::cin >> std::ws, name);
+    return name;
+}
 
+int getAge(std::string_view Name){
+    int age{};
+    std::cout << "Enter the age of " << Name << ": ";
+    std::cin >> age;
+    return age;
+}
+
+void WhichOlder(std::string_view firstPerson, int firstAge, std::string_view secondPerson, int secondAge)
+{
+    if (firstAge < secondAge)
+        std::cout << firstPerson << "(age " << firstAge << ")" << " is younger than " << secondPerson <<"(age " << secondAge << ")";
+    else if (firstAge > secondAge)
+    std::cout << firstPerson << "(age " << firstAge << ")" << " is older than " << secondPerson <<"(age " << secondAge << ")";
+}
+ 
 int main(){
-    double first{ getNumber() };
-    double second{ getNumber() };
-    char symbol{ getSymbol() };
-    
-    PrintResults(first, second, symbol);
+    const std::string firstName{ getName(1) };
+    const int firstAge{ getAge(firstName) };
+
+    const std::string secondName{ getName(2) };
+    const int secondAge{ getAge(secondName) };
+
+    WhichOlder(firstName, firstAge, secondName, secondAge);
+
+    return 0;
 }
